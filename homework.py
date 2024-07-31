ec9de687fb9b3264e28c7e3a99b0e94ad1868ea7
Dmitry Unitcaev <Dima.unitzaev@icloud.com>
2024-07-31 23:20:51 +0400

    Настроен 4 тест test_readable_function

--- homework.py
+++ homework.py
@@ -90,6 +90,7 @@ def test_find_suitable_user():
 # "Open Browser [Chrome]"
 
 
+
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
@@ -97,15 +98,21 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result =print_function_name_and_args(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_function_name_and_args(go_to_companyname_homepage,page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_function_name_and_args(find_registration_button_on_login_page,page_url,button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+def print_function_name_and_args(func, *args): # функция принимает имя функции и значения аргументов
+    func_name = func.__name__.replace('_', ' ').title()  # получаем имя функции и преобразуем его в читаемый вид (заменяем символ подчеркивания на пробел, делаем первую букву заглавной)
+    args_name = ", ".join([*args]) # преобразуем значения аргументов в строку
+    print(f"{func_name} [{args_name}]") # печатаем имя функции и значения аргументов
+    return f"{func_name} [{args_name}]" # возвращаем строку с именем функции и значениями аргументов
